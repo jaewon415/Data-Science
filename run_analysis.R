06fc96a24612efb685c0d8840f5c8b20a5cd4c03
Client Template <client@D-172-28-96-92.dhcp4.washington.edu>
2016-05-20 10:04:58 -0700

    basename

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,11 @@
 file.url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+temp.dir <- tempdir()
+setwd(temp.dir)
+downloaded <- download.file(file.url, basename(file.url))
+unzip <- unzip(zipfile = downloaded)
+read.table(file = "/UCI HAR Dataset/activity_labels.txt")
+
+downloaded <- download.file(file.url, temp.dir(tempfile()))
 tempfile <- tempfile()
 downloaded <- download.file(file.url, tempfile)
+
